f8f545d5c3a4f6e34e77376cf69750109aa33449
Sam Tobin-Hochstadt <samth@racket-lang.org>
2011-05-30 14:32:20 -0400

    ready to release

--- new-index.rkt
+++ new-index.rkt
@@ -25,6 +25,8 @@
 @(struct abstract paper ())
 @(struct preprint paper ())
 
+@(struct talk (title url loc date where))
+
 @(define MF @a[href: "http://www.ccs.neu.edu/home/matthias"]{Matthias Felleisen})
 @(define mflatt @a[href: "http://www.cs.utah.edu/~mflatt"]{Matthew Flatt})
 @(define stamourv @a[href: "http://www.ccs.neu.edu/home/stamourv"]{Vincent St-Amour})
@@ -128,7 +130,7 @@
 
 @(define analysis-papers
    (list 
-    (paper "Semantic Solutions to Program Analysis Problem"
+    (paper "Semantic Solutions to Program Analysis Problems"
            (list dvh)
            @a[href: "https://engineering.purdue.edu/~milind/pldi11fit/"]{Fun Ideas and Thoughts Session at the Conference on Programming Language Design and Implementation}
            "June 2011"
@@ -149,6 +151,85 @@
               "April 2011"
               `(["PDF" "js-modules.pdf"]))))
 
+@(define macro-talks
+   (list 
+     (talk "Languages as Libraries"
+               "mit-may-2011.pdf"
+               @a[href: "http://www.csail.mit.edu/events/eventcalendar/calendar.php?show=event&id=2978"]{MIT
+  CSAIL Programming Languages Working Group}
+               "Cambridge, MA" "May 2011")
+     (talk "Extensible Pattern Matching in an Extensible Language"
+           "ifl2010-slides.pdf"
+           @a[href: "http://www.cs.uu.nl/wiki/bin/view/IFL2010/WebHome"]{Implementation and Application of Functional Languages}
+           "Alphen an der Rijn, Netherlands" "September 2010")
+    ))
+
+@(define tr-talks
+   (list
+    (talk "Growing Software: From Scripts to Programs"
+          "osu-march-2011.pdf"
+           @a[href: "http://eecs.oregonstate.edu/graduate/colloquium/"]{Oregon State University EECS Colloquium}
+           "Corvallis, Oregon" "March 2011")
+    (talk "From Scripts to Programs"
+          #f
+          "Indiana University Department of Computer Science"
+          "Bloomington, IN" "November 2010")
+    (talk "Logical Types for Untyped Languages"
+          "icfp-2010-slides.pdf"
+           @a[href: "http://www.icfpconference.org/icfp2010/index.html"]{International Conference on Functional Programming}
+           "Baltimore, MD" "September 2010")
+    (talk "Logical Types for Scheme"
+          "nepls-2010-slides.pdf"
+           @a[href: "http://nepls.org/Events/24/"]{New England Programming Languages and Systems Symposium}
+           "Yale University" "April 2010")
+    (talk "Typed Scheme: From Scripts to Programs"
+          #f
+          @span{@a[href: "http://www.eecs.harvard.edu/"]{Harvard University}}
+           "Cambridge, MA" "February 2010")
+    (talk "Types for Scheme, in Scheme" #f
+             @a[href: "http://fare.livejournal.com/149685.html"]{Boston Lisp Meeting}
+          "Boston, MA" "December 2009")
+    (talk "Typed Scheme: From Scripts to Programs"
+          "defense-slides.pdf"
+           @span{Dissertation Defense, @a[href: "http://www.ccs.neu.edu/"]{Northeastern University}}
+           "Boston, MA" "December 2009")
+    (talk "Typed Scheme: From Scripts to Programs"
+          #f
+          "Sun Microsystems Laboratories"
+          "Burlington, MA" "September 2009")
+    (talk "The Design and Implementation of Typed Scheme"
+          "mitchfest-slides.pdf"
+          @a[href: "http://www.ccs.neu.edu/events/wand-symposium/"]{Symposium in
+          Honor of Mitchell Wand}
+          "Boston, MA" "August 2009")
+
+    (talk "Cycles without pollution: a gradual typing poem"
+          "stop09-slides.pdf"
+          @a[href: "http://wrigstad.com/stop09/"]{1st International Workshop on Script to Program Evolution}
+          "Genoa, Italy" "July 2009")
+
+    (talk "To Type or Not To Type"
+          #f
+          @span{@a[href: "http://acm.ccs.neu.edu"]{Northeastern University ACM Speaker Series}}
+           "Boston, MA" "April 2008")
+    (talk "Typed Scheme"
+          #f
+          @span{@a[href: "http://www.ccs.neu.edu"]{Northeastern University CCIS Open House}}
+           "Boston, MA" "March 2008")
+    (talk "The Design and Implementation of Typed Scheme"
+          #f
+          @a[href: "http://www.cs.ucsd.edu/popl/08/"]{Symposium on Principles of Programming Languages (POPL)}
+          "San Fransisco, CA" "January 2008")
+    (talk "Types for Untyped Languages"
+          #f
+          @span{@a[href: "http://www.ccs.neu.edu"]{Northeastern University CCIS Open House}}
+           "Boston, MA" "March 2007")
+    (talk "Interlanguage Migration: From Scripts to Programs"
+          "dls-slides.pdf"
+          @a[href: "http://www.dcl.hpi.uni-potsdam.de/dls2006/openconf.php"]{Dynamic Languages Symposium (DLS)}
+          "Portland, OR" "October 2006")
+   ))
+
 
 @(define (format-coauthors cos)
    (match cos
@@ -166,7 +247,7 @@
    @p[class: 'pub]{
     @span[class: 'title (format-title pr)].
     @(format-coauthors (paper-co pr)) @~
-    @(paper-loc pr) @(if (equal? "" (paper-loc pr)) "" ",") @(paper-date pr). @~
+    @(paper-loc pr)@(if (equal? "" (paper-loc pr)) "" ",") @(paper-date pr). @~
     @list["[ "]
     @(add-between 
         (for/list ([r (paper-resources pr)])
@@ -175,8 +256,16 @@
     @list[" ]"]
     })
 
+@(define (format-talk t)
+  @p[class: 'pub]{
+    @(if (talk-url t) @a[href: (talk-url t) class: 'talktitle (talk-title t)] (talk-title t)),
+    @(list (talk-loc t) ", " (talk-date t) ", " (talk-where t) ".")})
+
 @(define (papers ps)
    @div{@h2[class: 'subproject]{Papers} @(map format-paper ps)})
+
+@(define (talks ps)
+   @div{@h2[class: 'subproject]{Talks} @(map format-talk ps)})
 
 @(define racket @a[href: "http://racket-lang.org"]{Racket})
 
@@ -213,13 +302,22 @@ function toggleBibTeX(elt) {
 
 @box['name sth]{
  @p{@img[id: "photo" src: "tree.jpg" alt: "Tree" title:"Winter"]
+   @;@img[id: "photo" width: "200" src: "plt-logo-red-diffuse.png" alt: "Racket" title:"Racket"]
     @p{@a[href: "https://mozillalabs.com/"]{Mozilla Research Fellow} @~
-       @a[href: "http://www.ccs.neu.edu/scheme/"]{PLT} @quote["@"]
+       @a[href: "http://www.ccs.neu.edu/scheme/"]{PLT} @"@"
        @a[href: "http://www.ccs.neu.edu/research/prl/"]{Programming Research Lab}@~
        @a[href: "http://www.ccs.neu.edu/"]{College of Computer and Information Science}@~
        @a[href: "http://www.northeastern.edu/"]{Northeastern University}}}
  @p{Office: @a[href:"http://www.northeastern.edu/campusmap/"]{West Village H}, Room 308@~
-    Email: @a[href:"mailto:samth@ccs.neu.edu" "samth@ccs.neu.edu"]@~}}
+    Email: @a[href:"mailto:samth@ccs.neu.edu" "samth@ccs.neu.edu"]@~
+    Microblogging: @a[href: "http://twitter.com/samth/" "@samth"]}
+ @p{Papers: @a[href: "http://www.informatik.uni-trier.de/~ley/db/indices/a-tree/t/Tobin=Hochstadt:Sam.html"]{DBLP}, 
+            @a[href: "http://arxiv.org/a/tobinhochstadt_s_1"]{arXiv},
+            @a[href: "http://portal.acm.org/author_page.cfm?id=81319502825"]{ACM}}
+ @p{Come to @a[href: "http://con.racket-lang.org/"]{RacketCon}!
+    Submit to @a[href: "http://scheme2011.ucombinator.org/"]{Scheme 2011}! @~}
+}
+
 
 @box['research "Research Projects"]{
 
@@ -235,6 +333,7 @@ function toggleBibTeX(elt) {
         existing untyped Racket programs to be enriched with
       types.}
 @(papers tr-papers)
+@(talks tr-talks)
 }
 
 @project['metaprogramming "Building Languages"]{
@@ -242,7 +341,7 @@ function toggleBibTeX(elt) {
       programming language that allows developers to create
       custom languages for everything from pattern matching
       to type checking.}
-@(papers macro-papers)}
+@(papers macro-papers) @(talks macro-talks)}
 
 @project['analysis "Analysis and Verification"]{
       @pdesc{I am developing  analysis and verification
@@ -251,7 +350,7 @@ function toggleBibTeX(elt) {
 
 @project['javascript "JavaScript"]{
  @pdesc{In collaboration
-      with @a[href:"https://mozillalabs.com/"]{Mozilla Research}, I
+      with @a[href:"https://mozillalabs.com/"]{Mozilla Research} and ECMA Technical Committee 39, I
       am working on the next version of the JavaScript language,
       focusing on making JavaScript an effective language for building
       large-scale web applications.}
@@ -275,6 +374,15 @@ function toggleBibTeX(elt) {
     computing.}}
      @(papers fortress-papers)
      }
+}
+
+@box['activities "Activities"]{
+@p{@a[href: "http://wrigstad.com/stop/"]{STOP 2012}, Chair}
+@p{@a[href: "http://scheme2011.ucombinator.org/"]{Scheme Workshop 2011}, Program Committee}
+@p{@a[href: "http://nepls.org/Events/25"]{NEPLS 25}, Chair}
+@p{@a[href: "http://www.mpi-sws.org/~dreyer/tldi2011/"]{TLDI 2011}, Program Committee}
+@p{@a[href: #f]{FOOL 2010}, Program Committee}
+@p{@a[href: #f]{TFP 2010}, Program Committee}
 }
 
 @box['teaching "Teaching"]{ 
